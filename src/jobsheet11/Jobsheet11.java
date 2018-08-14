81d102581898d8704ac734c57d98d41d20bddd9a
ASUS <ASUS@LAPTOP-GS0UURLT>
2018-08-14 08:58:45 +0700

    Initial Commit

---
+++ src/jobsheet11/Jobsheet11.java
@@ -0,0 +1,11 @@
+
+package jobsheet11;
+import java.util.Locale;
+
+public class Jobsheet11 {
+
+    public static void main(String[] args) {
+        String  identitas = "muhammad hafid azis / XRPL2 / 23";
+        System.out.println("Identitas : "+ identitas);
+        
+       
