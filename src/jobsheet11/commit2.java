9232acafba4351a951b62194b3c9bbeaff685878
ASUS <ASUS@LAPTOP-GS0UURLT>
2018-08-15 09:54:18 +0700

    Tambah Tampil Per Kata.

--- src/jobsheet11/commit2.java
+++ src/jobsheet11/commit2.java
@@ -38,8 +38,18 @@ public class commit2 {
         return kalimat;
     }
     String convert = vocal2Angka(kalimat);
+    
+    private static void tampilPerKata(String kalimat, String convert){
+        String[]arrKal = kalimat.split(" ");
+        String[]arrcon = convert.split(" ");
+        
+        for(int i=0;i<arrKal.length;i++)
+            System.out.println(arrKal[i]+" => "+arrcon[i]);
+        
+         tampilPerKata(kalimat,convert);
         
     
+}
 }
 
 
