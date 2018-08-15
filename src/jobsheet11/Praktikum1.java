027746edf6c46a8c5144dcd5a53babb945305ef6
ASUS <ASUS@LAPTOP-GS0UURLT>
2018-08-15 09:20:12 +0700

    Initial Commit

---
+++ src/jobsheet11/Praktikum1.java
@@ -0,0 +1,11 @@
+
+package jobsheet11;
+import java.util.Locale;
+
+public class Praktikum1 {
+    public static void main(String[] args) {
+        String  identitas = "muhammad hafid azis / XRPL2 / 23";
+        System.out.println("Identitas : "+ identitas);
+    
+}
+}
