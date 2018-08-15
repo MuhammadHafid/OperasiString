86f5a455ba98fee732be3fab6d11cf5e0c854488
ASUS <ASUS@LAPTOP-GS0UURLT>
2018-08-15 09:47:17 +0700

    Initial Commit.

---
+++ src/jobsheet11/commit2.java
@@ -0,0 +1,19 @@
+
+package jobsheet11;
+
+
+public class commit2 {
+     private static void tampilJudul(String identitas){
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    
+    public static void main(String[] args){
+    
+        String identitas = " Muhammad hafid azis / X RPL 2 / 23 ";
+        
+        tampilJudul(identitas); 
+    
+}
+}
