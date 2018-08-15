5c1cf01762c62707dc2f08c7507e688e891772d0
ASUS <ASUS@LAPTOP-GS0UURLT>
2018-08-15 09:24:58 +0700

    Coba method length & isEmpty.

--- src/jobsheet11/Praktikum1.java
+++ src/jobsheet11/Praktikum1.java
@@ -6,6 +6,17 @@ public class Praktikum1 {
     public static void main(String[] args) {
         String  identitas = "muhammad hafid azis / XRPL2 / 23";
         System.out.println("Identitas : "+ identitas);
+        
+          String x = "operasi";
+        System.out.println("isi variable x : "+ x);
+        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variable y : "+ y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
+        
     
 }
 }
